b8caf02e5498f1e8e6b8229bd89c4d35ad4c8a5c
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-04-26 13:02:20 +0800

    懒加载的几种写法

--- Swift_lazy_swift/Swift_lazy_swift/ViewController.swift
+++ Swift_lazy_swift/Swift_lazy_swift/ViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 让数组在用到的时候再加载 节省资源
+    // 懒加载 用到时再加载 多次使用才会加载一次
+    lazy var names:[String] = {
+    
+        let names = ["tian","bin","bin"]
+        
+        print("验证是否是懒加载")
+        
+        return names
+    
+    }()
+    
+    lazy var btn:UIButton = {
+    
+        let btn = UIButton()
+        
+        btn .setTitle("nihao", for: .normal)
+        
+        return btn
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        print(names.count)
+        print(btn)
     }
+
 
 
 }
