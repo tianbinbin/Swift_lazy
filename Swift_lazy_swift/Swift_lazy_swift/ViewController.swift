cd6d4d22c59edf86f49be41d26521aba7cf70942
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-04-26 12:52:46 +0800

    上一创建成oc 的了 这个才是

---
+++ Swift_lazy_swift/Swift_lazy_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_lazy_swift
+//
+//  Created by 田彬彬 on 2017/4/26.
+//  Copyright © 2017年 田彬彬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
